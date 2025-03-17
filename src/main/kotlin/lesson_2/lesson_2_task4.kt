44db6696b4df47bfe5e3a015f1702d8e19b74dce
lie_feld <a.musledinova@gmail.com>
2025-03-17 14:59:56 +0700

    KS-2-4. Task 4 - val percents added

--- src/main/kotlin/lesson_2/lesson_2_task4.kt
+++ src/main/kotlin/lesson_2/lesson_2_task4.kt
@@ -5,9 +5,10 @@ fun main() {
     val minedCrystalOre = 7
     val minedIronOre = 11
     val oneTimeBuffCoefficient = 20
+    val percents = 100
 
-    val crystalOreWithBuff = minedCrystalOre * oneTimeBuffCoefficient / 100
-    val ironOreWithBuff = minedIronOre * oneTimeBuffCoefficient / 100
+    val crystalOreWithBuff = minedCrystalOre * oneTimeBuffCoefficient / percents
+    val ironOreWithBuff = minedIronOre * oneTimeBuffCoefficient / percents
 
     println("Buff value for mined crystal ore: $crystalOreWithBuff")
     println("Buff value for mined iron ore: $ironOreWithBuff")
