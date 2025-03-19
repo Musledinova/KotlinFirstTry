d3cd65c0b352615dccb0e2992161a7788d302db4
lie_feld <a.musledinova@gmail.com>
2025-03-19 20:53:45 +0700

    KS-3-5. Task 5 - solution fixes

--- src/main/kotlin/lesson_3/lesson_3_task5.kt
+++ src/main/kotlin/lesson_3/lesson_3_task5.kt
@@ -4,10 +4,8 @@ fun main() {
 
     val serverResponse = "D2-D4;0"
 
-    val moveCoordinates = serverResponse.split(";")
-
-    val (moveFrom, moveTo) = moveCoordinates[0].split("-")
-    val moveNumber = serverResponse.split(";")[1]
+    val (moveFrom, moveTo, moveNumber) = serverResponse
+        .split(";", "-")
 
     println(moveFrom)
     println(moveTo)
