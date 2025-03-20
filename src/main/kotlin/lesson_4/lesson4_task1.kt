32e872688d78d6c2834ad91a9150298b91b1e8af
lie_feld <a.musledinova@gmail.com>
2025-03-20 15:18:37 +0700

    KS-4-1. Task 1 - solution fixes

--- src/main/kotlin/lesson_4/lesson4_task1.kt
+++ src/main/kotlin/lesson_4/lesson4_task1.kt
@@ -2,14 +2,14 @@ package org.example.lesson_4
 
 fun main() {
 
-    val totalTables = 13
-
     val tablesBookedToday = 13
     val tablesBookedTomorrow = 9
 
     println(
-        "[Доступность столиков на сегодня: ${tablesBookedToday < totalTables}]" +
-                "\n[Доступность столиков на завтра: ${tablesBookedTomorrow < totalTables}]"
+        "[Доступность столиков на сегодня: ${tablesBookedToday < TOTAL_TABLES}]" +
+                "\n[Доступность столиков на завтра: ${tablesBookedTomorrow < TOTAL_TABLES}]"
     )
 
 }
+
+const val TOTAL_TABLES = 13
