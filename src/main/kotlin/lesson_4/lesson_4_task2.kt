5397bd5a95984821dfc0775e4e2e8eb3a6916f6b
lie_feld <a.musledinova@gmail.com>
2025-03-21 10:28:38 +0700

    KS-4-2. Task 2 - formatting fixes

--- src/main/kotlin/lesson_4/lesson_4_task2.kt
+++ src/main/kotlin/lesson_4/lesson_4_task2.kt
@@ -9,15 +9,15 @@ fun main() {
     val secondCargoValue = 100
 
     println(
-        "Груз с весом $firstCargoWeight кг и объемом $firstCargoValue л соответствует категории '$AVERAGE_CATEGORY': " +
-                "${
-                    (firstCargoWeight > AVERAGE_CATEGORY_WEIGHT_FROM) && (firstCargoWeight <= AVERAGE_CATEGORY_WEIGHT_TO)
-                            && (firstCargoValue <= AVERAGE_CATEGORY_VALUE)
-                } \n" +
-                "Груз с весом $secondCargoWeight кг и объемом $secondCargoValue л соответствует категории '$AVERAGE_CATEGORY': ${
-                    (secondCargoWeight > AVERAGE_CATEGORY_WEIGHT_FROM) && (secondCargoWeight <= AVERAGE_CATEGORY_WEIGHT_TO)
-                            && (secondCargoValue <= AVERAGE_CATEGORY_VALUE)
-                }"
+        "Груз с весом $firstCargoWeight кг и объемом $firstCargoValue л соответствует категории '$AVERAGE_CATEGORY': " + "${
+            (firstCargoWeight > AVERAGE_CATEGORY_WEIGHT_FROM) &&
+                    (firstCargoWeight <= AVERAGE_CATEGORY_WEIGHT_TO) &&
+                    (firstCargoValue <= AVERAGE_CATEGORY_VALUE)
+        } \n" + "Груз с весом $secondCargoWeight кг и объемом $secondCargoValue л соответствует категории '$AVERAGE_CATEGORY': ${
+            (secondCargoWeight > AVERAGE_CATEGORY_WEIGHT_FROM) &&
+                    (secondCargoWeight <= AVERAGE_CATEGORY_WEIGHT_TO) &&
+                    (secondCargoValue <= AVERAGE_CATEGORY_VALUE)
+        }"
     )
 
 }
