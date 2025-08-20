68e5865477b91c3a196c2e0fa5c3730e3ad8d3d1
lie_feld <a.musledinova@gmail.com>
2025-08-20 14:11:21 +0700

    KS-12-1 Task 1 - comment fixes

--- src/main/kotlin/lesson_12/lesson12_task1.kt
+++ src/main/kotlin/lesson_12/lesson12_task1.kt
@@ -1,19 +1,9 @@
 package org.example.lesson_12
 
-class DailyWeather() {
+class DailyWeather {
     var dayTemperature = "+40"
     var nightTemperature = "-10"
     var isPrecipitation = false
-
-    constructor(
-        dayTemperature: String,
-        nightTemperature: String,
-        isPrecipitation: Boolean,
-    ) : this() {
-        this.dayTemperature = dayTemperature
-        this.nightTemperature = nightTemperature
-        this.isPrecipitation = isPrecipitation
-    }
 
     fun printDailyWeather() {
         println("Погода днем: $dayTemperature, погода ночью: $nightTemperature, впадание осадков: $isPrecipitation")
@@ -21,8 +11,15 @@ class DailyWeather() {
 }
 
 fun main() {
-    val dayOne = DailyWeather("+24", "+10", false)
-    val dayTwo = DailyWeather("+22", "+3", true)
+    val dayOne = DailyWeather()
+    dayOne.dayTemperature = "+24"
+    dayOne.nightTemperature = "+10"
+    dayOne.isPrecipitation = false
+
+    val dayTwo = DailyWeather()
+    dayTwo.dayTemperature = "+22"
+    dayTwo.nightTemperature = "+3"
+    dayTwo.isPrecipitation = true
 
     dayOne.printDailyWeather()
     dayTwo.printDailyWeather()
